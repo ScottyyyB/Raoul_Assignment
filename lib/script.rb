434060cc0cecd94660f86d3b14107bf92513e1a0
ScottB <17sb@cis.dk>
2017-10-30 17:56:34 +0100

    Actual Finish with RSPEC Test Working With Print

--- lib/script.rb
+++ lib/script.rb
@@ -1,4 +1,4 @@
 def array_Multiplier(arr, multiplier)
-  arr.each { |num| puts num *= multiplier }
+  arr.each { |num| print num *= multiplier }
 end
-array_Multiplier([1, 2, 3], 4)
+array_Multiplier([1, 2, 3], 2)
--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -1,7 +1,9 @@
 require './lib/script.rb'
 
 describe '#array_Multiplier' do
-    it 'modifies the given array by the given multiplier and returns it' do
-      expect(array_Multiplier([1,2,3],2)).to eq "array modified to #{[2,4,6]}"
-  end
+    it 'prints the value of each number' do
+      expect do
+      (array_Multiplier([1,2,3],2))
+  end.to output("246").to_stdout
+end
 end
